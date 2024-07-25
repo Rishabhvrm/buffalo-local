2378207b1e799f7c71ae62d4b6025d070a3974c5
Rishabh <rishabhvrm43@gmail.com>
2024-07-25 14:48:15 -0700

    refactor: relative path for brand image URL

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -5,7 +5,8 @@ export function Brand() {
   return (
     <Flex alignItems="center" mb={"2%"}>
       <Image 
-      src={`${process.env.REACT_APP_UI_URL}/assest/images/buffalo-2.jpeg`}
+      // src={`${process.env.REACT_APP_UI_URL}/assest/images/buffalo-2.jpeg`}
+      src={`/assest/images/buffalo-2.jpeg`}
       alt="logo" width="20%" height="20%" rounded={"full"}/>
       <Box ml="5%" >
         <Heading color="blue.900">Buffalo Local</Heading>
